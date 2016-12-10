4ab1f6f53dd4e17ddc37616e8a4aa82cb646c138
justmartin <justin.lamar.martin@gmail.com>
2016-12-09 22:42:08 -0800

    Modified mobile nav bar styling. Modified column widths for collapsible info sections.

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 
-	$(".button-collapse").sideNav();
+	$(".button-collapse").sideNav({
+      menuWidth: 250, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    });
 
 	$(window).scroll(function() {
     var scrollPos = $(window).scrollTop(),
