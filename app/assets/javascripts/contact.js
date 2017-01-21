894890d3619c63527f70b8b4184305a625f4b1dc
justmartin <justin.lamar.martin@gmail.com>
2017-01-21 11:28:28 -0800

    Added js hover function for adding contact image opacity.

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
-	
+	$('.contact-image').hover(function() {
+		$(this).addClass('contact-image-opacity');
+	},
+		function() {
+		$(this).removeClass('contact-image-opacity');
+	});
 
 });
